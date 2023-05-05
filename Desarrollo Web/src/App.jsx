2cc08857f3cec430344d039ed150dad003b69a61
Rodsgl <rodrigo.sgl19@gmail.com>
2023-05-04 22:45:38 -0400

    aaaa

--- Desarrollo Web/src/App.jsx
+++ Desarrollo Web/src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import {
+  Box,
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
+  Collapse,
   Dialog,
   DialogContent,
   DialogTitle,
@@ -14,20 +16,69 @@ import {
   ImageList,
   ImageListItem,
   ImageListItemBar,
+  Popper,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import axios from "axios";
 import CircularProgress from "@mui/material/CircularProgress";
 import BackspaceIcon from "@mui/icons-material/Backspace";
 import SportsKabaddiIcon from "@mui/icons-material/SportsKabaddi";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import HeartBrokenIcon from "@mui/icons-material/HeartBroken";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { styled } from "@mui/material/styles";
+import { loremIpsum } from "lorem-ipsum";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 function App() {
   const [dog, setDog] = useState("");
   const [nameDog, setNameDog] = useState("");
+  const [descriptionDog, setDescriptionDog] = useState("");
   const [listaAceptado, setListaAceptado] = useState([]);
   const [listaRechazado, setListaRechazado] = useState([]);
-  const [dataDog, setDataDog] = useState([{ image: "", name: "" }]);
+  const [dataDog, setDataDog] = useState([{ image: "", name: "", desc: "" }]);
   const [spinner, setSpinner] = useState(true);
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const generarLorem = () => {
+    let lorem = "";
+    lorem = loremIpsum({
+      count: 1, // Number of "words", "sentences", or "paragraphs"
+      format: "plain", // "plain" or "html"
+      paragraphLowerBound: 3, // Min. number of sentences per paragraph.
+      paragraphUpperBound: 7, // Max. number of sentences per paragarph.
+      random: Math.random, // A PRNG function
+      sentenceLowerBound: 5, // Min. number of words per sentence.
+      sentenceUpperBound: 15, // Max. number of words per sentence.
+      units: "sentences", // paragraph(s), "sentence(s)", or "word(s)"
+    });
+    setDescriptionDog(lorem);
+  };
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handlePopper = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popper" : undefined;
 
   const imagenesPerro = () => {
     setSpinner(true);
@@ -39,6 +90,7 @@ function App() {
   useEffect(() => {
     imagenesPerro();
     nombrePerro();
+    generarLorem();
   }, []);
 
   const nombrePerro = () => {
@@ -55,7 +107,7 @@ function App() {
   };
 
   useEffect(() => {
-    setDataDog({ image: dog, name: nameDog });
+    setDataDog({ image: dog, name: nameDog, desc: descriptionDog });
   }, [dog]);
   //console.log(dataDog);
 
@@ -64,6 +116,7 @@ function App() {
     setListaAceptado((listaAceptado) => [...listaAceptado, item]);
     imagenesPerro();
     nombrePerro();
+    generarLorem();
     //console.log(listaAceptado);
   };
 
@@ -71,6 +124,7 @@ function App() {
     setListaRechazado((listaRechazado) => [...listaRechazado, item]);
     imagenesPerro();
     nombrePerro();
+    generarLorem();
   };
   //console.log(listaRechazado);
 
@@ -97,8 +151,125 @@ function App() {
         Tinder de Perro
       </Typography>
       <hr />
-      <Grid container spacing={8}>
-        <Grid item md={4} xs={4}>
+      <Grid container spacing={2}>
+        <Grid item md={4} sm={4} xs={12}>
+          <Typography gutterBottom variant="h5" component="div">
+            Candidato
+          </Typography>
+          <Card sx={{ maxWidth: 400, minWidth: 400 }}>
+            <CardMedia
+              sx={{ height: 400, width: 400 }}
+              component="img"
+              src={dog}
+              title="Perro Random"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {nameDog}
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <Tooltip title="Rechazar" placement="bottom">
+                <IconButton
+                  disabled={spinner}
+                  sx={{ color: "rgba(0, 131, 208, 0.54)" }}
+                  aria-label="no tú no"
+                  onClick={() => rechazarPerro(dataDog)}
+                >
+                  <HeartBrokenIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Aceptar" placement="bottom">
+                <IconButton
+                  disabled={spinner}
+                  sx={{ color: "rgba(232, 18, 36, 0.54)" }}
+                  aria-label="no tú no"
+                  onClick={() => aceptarPerro(dataDog)}
+                >
+                  <FavoriteIcon />
+                </IconButton>
+              </Tooltip>
+
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <Tooltip title="Descripción" placement="top">
+                  <ExpandMoreIcon />
+                </Tooltip>
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Descripción.</Typography>
+                <Typography paragraph>{descriptionDog}</Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </Grid>
+        <Grid item md={4} sm={4} xs={12}>
+          <Typography gutterBottom variant="h5" component="div">
+            Aceptados
+          </Typography>
+          <Card sx={{ maxWidth: 400, minWidth: 400, minHeight: 500 }}>
+            <ImageList
+              sx={{ width: 400, height: 500 }}
+              cols={3}
+              rowHeight={164}
+            >
+              {listaAceptado.map((item, index) => (
+                <ImageListItem key={index}>
+                  <img
+                    src={`${item.image}?w=164&h=164&fit=crop&auto=format`}
+                    srcSet={`${item.image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  />
+                  <ImageListItemBar
+                    title={item.name}
+                    subtitle={
+                      <Tooltip title="Descripción" placement="top">
+                        <IconButton
+                          sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                          aria-describedby={index}
+                          aria-label="descripción"
+                          onClick={handlePopper}
+                        >
+                          <VisibilityIcon />
+                        </IconButton>
+                      </Tooltip>
+                    }
+                    actionIcon={
+                      <>
+                        <Tooltip title="no, mejor no" placement="top">
+                          <IconButton
+                            sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                            aria-label="no, tú no"
+                            onClick={() => sacarAceptado(item)}
+                          >
+                            <BackspaceIcon />
+                          </IconButton>
+                        </Tooltip>
+                        <Popper id={index} open={open} anchorEl={anchorEl}>
+                          <Box
+                            sx={{
+                              border: 1,
+                              p: 1,
+                              bgcolor: "#1a1a1a",
+                            }}
+                          >
+                            {item.desc}
+                          </Box>
+                        </Popper>
+                      </>
+                    }
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Card>
+        </Grid>
+        <Grid item md={4} sm={4} xs={12}>
           <Typography gutterBottom variant="h5" component="div">
             Rechazados
           </Typography>
@@ -122,85 +293,46 @@ function App() {
                   />
                   <ImageListItemBar
                     title={item.name}
-                    subtitle={"descripción"}
-                    actionIcon={
-                      <IconButton
-                        sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                        aria-label="dele pah elante nomás"
-                        onClick={() => sacarRechazado(item)}
-                      >
-                        <SportsKabaddiIcon />
-                      </IconButton>
+                    subtitle={
+                      <Tooltip title="Descripción" placement="top">
+                        <IconButton
+                          sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                          aria-describedby={index}
+                          aria-label="descripción"
+                          onClick={handlePopper}
+                        >
+                          <VisibilityIcon />
+                        </IconButton>
+                      </Tooltip>
                     }
-                  />
-                </ImageListItem>
-              ))}
-            </ImageList>
-          </Card>
-        </Grid>
-        <Grid item md={4} xs={4}>
-          <Typography gutterBottom variant="h5" component="div">
-            Candidato
-          </Typography>
-          <Card sx={{ maxWidth: 400, minWidth: 400 }}>
-            <CardMedia
-              sx={{ height: 400, width: 400 }}
-              component="img"
-              src={dog}
-              title="Perro Random"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {nameDog}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                disabled={spinner}
-                onClick={() => rechazarPerro(dataDog)}
-                size="small"
-              >
-                Rechazar
-              </Button>
-              <Button
-                disabled={spinner}
-                onClick={() => aceptarPerro(dataDog)}
-                size="small"
-              >
-                Aceptar
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item md={4} xs={4}>
-          <Typography gutterBottom variant="h5" component="div">
-            Aceptados
-          </Typography>
-          <Card sx={{ maxWidth: 400, minWidth: 400, minHeight: 500 }}>
-            <ImageList
-              sx={{ width: 400, height: 500 }}
-              cols={3}
-              rowHeight={164}
-            >
-              {listaAceptado.map((item, index) => (
-                <ImageListItem key={index}>
-                  <img
-                    src={`${item.image}?w=164&h=164&fit=crop&auto=format`}
-                    srcSet={`${item.image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                  />
-                  <ImageListItemBar
-                    title={item.name}
-                    subtitle={"descripción"}
                     actionIcon={
-                      <IconButton
-                        sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                        aria-label="no tú no"
-                        onClick={() => sacarAceptado(item)}
-                      >
-                        <BackspaceIcon />
-                      </IconButton>
+                      <>
+                        <Tooltip
+                          title="sí, la verdad es que sí"
+                          placement="top"
+                        >
+                          <IconButton
+                            sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                            aria-label="dele pah elante nomás"
+                            onClick={() => sacarRechazado(item)}
+                          >
+                            <SportsKabaddiIcon />
+                          </IconButton>
+                        </Tooltip>
+                        <Popper id={id} open={open} anchorEl={anchorEl}>
+                          <Box
+                            sx={{
+                              border: 1,
+                              p: 1,
+                              bgcolor: "#1a1a1a",
+                            }}
+                          >
+                            {item.desc}
+                          </Box>
+                        </Popper>
+                      </>
                     }
-                  />
+                  ></ImageListItemBar>
                 </ImageListItem>
               ))}
             </ImageList>
